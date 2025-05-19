b8e082613f8a8d81f6b84a9bf7938e0c4f1701b3
Darshan <darshansrinivasa034@gmail.com>
2025-05-19 23:07:50 +0530

    Practice inline styling, apply text color absed on the hour of the day.

--- my-react-app/src/index.jsx
+++ my-react-app/src/index.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+let color;
+let size = '40px';
+let text;
+
+const time = new Date().getHours();
+if (time > 0 && time < 12) {
+    color = 'red';
+    text = 'Good Morning';
+} else if (time >= 12 && time < 18) {
+    color = 'green';
+    text = 'Good Afternoon';
+} else {
+    color = 'blue';
+    text = 'Good Night';
+}
+const customStyle = {
+    color: color,
+    fontSize: size
+};
+
 const root = createRoot(document.getElementById('root'));
 root.render(
     <div>
-        <h1 className="heading" contentEditable="true" spellCheck="true">Favourite Foods</h1>
-        <ul>
-            <li>Pizza</li>
-            <li>Pasta</li>
-            <li>Salad</li>
-        </ul>
+        <h1 style={customStyle}>
+            {text}
+        </h1>
     </div>
 );
